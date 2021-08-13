c9d49faceea3b5d19535a9c221ebdc2faaca9bca
Henit Chobisa <henit.chobisa2020@vitstudet.ac.in>
2021-08-13 18:52:25 +0530

    Service.plist bug resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trikon_app/Authentication/loginPage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp();
   runApp(loadApp());
 }
 
 class loadApp extends StatefulWidget {
   const loadApp({Key? key}) : super(key: key);
-
   @override
   _loadAppState createState() => _loadAppState();
 }
